53c98f813329c4a1cb21499d670dfa1f3531d034
chrilona <christabelalona@gmail>
2022-06-22 10:07:56 +0300

    first commit

---
+++ exercise.py
@@ -0,0 +1,23 @@
+# Given a python function named smallest that accepts 
+#a list of unsorted intergers and returns the smallest number in the list
+# def smallest():
+#     my_list=[78,56,67,45,2,45,10,3]
+#     small=my_list[0]
+#     for i in my_list:
+#         if i < small:
+#             small=i
+#     print(small)
+# smallest()
+#Take a string and return half its length in uppercase
+text="My mother is kind"
+half_text=len(text)//2
+half_text2=text[0:half_text].upper()
+print(half_text2)
+# # nested loops
+# for i in ("Hello","Dear"):
+#     for j in ("Good","Bye"):
+#         print(i,j)
+
+# for a in range(1,4):
+#     for b in range(3,6):
+#         print(a*b,end="\n")
---
+++ hangman.py
@@ -0,0 +1,46 @@
+import random
+import string
+from words import words
+
+def get_words(words):
+    word=random.choice(words) 
+    while "-" in word or " " in words:
+        word=random.choice(words)
+    return word   
+
+def play_hangman():
+    word=get_words(words)
+    word_letters=set(word)
+    # alphabet=set(string.ascii_uppercase)
+    used_letters=set()
+    lives=6
+    
+    while len(word_letters)>0 and lives>0 :
+
+        print("You have ",lives ,"left and you have used these letters:", " ".join(used_letters))
+        word_list=[letter if letter in used_letters else "-" for letter in word] 
+        print("Current word: "," ".join(word_list))
+
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+            else:
+                lives=lives - 1
+                print("Letter is not in the word")
+
+        elif user_letter in used_letters:
+            print(f"You have already used {user_letter}.Please try again ")
+
+        else:
+            print("Invalid character.TRY again")
+
+        if lives==0:
+         print("Sorry you died.The word was" ,word)
+        else:
+         print("Your guess was correct" ,word, "!! Congratulations" )      
+
+
+user_input=input("Guess a letter: ")
